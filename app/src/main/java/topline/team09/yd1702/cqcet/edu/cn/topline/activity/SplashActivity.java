2fd7ca9bd585dfe0cfb866403bc5b23f8af98d3b
SunRedSoldier <15213699752@163.com>
2019-10-21 14:22:24 +0800

    first commit

---
+++ app/src/main/java/topline/team09/yd1702/cqcet/edu/cn/topline/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package topline.team09.yd1702.cqcet.edu.cn.topline.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import topline.team09.yd1702.cqcet.edu.cn.topline.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/test/java/topline/team09/yd1702/cqcet/edu/cn/topline/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package topline.team09.yd1702.cqcet.edu.cn.topline;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
