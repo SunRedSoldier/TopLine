804bd7c07b937e54b136b231ef273471c3e964fc
SunRedSoldier <15213699752@163.com>
2019-10-24 10:33:58 +0800

    欢迎模块

--- app/src/main/java/topline/team09/yd1702/cqcet/edu/cn/topline/activity/SplashActivity.java
+++ app/src/main/java/topline/team09/yd1702/cqcet/edu/cn/topline/activity/SplashActivity.java
@@ -1,8 +1,13 @@
 package topline.team09.yd1702.cqcet.edu.cn.topline.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import topline.team09.yd1702.cqcet.edu.cn.topline.MainActivity;
 import topline.team09.yd1702.cqcet.edu.cn.topline.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -11,5 +16,21 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        init();
+    }
+
+    private void init() {
+        //利用Timer让此界面延迟3s后再跳转
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+        //设置该task在延迟3秒后执行
+        timer.schedule(task,3000);
     }
 }
